7a4c2d29ce664fae9813d518bba14f4463dab4b0
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-04-21 17:35:43 +0300

    Update calculate.hpp

--- calculate.hpp
+++ calculate.hpp
@@ -1,12 +1,11 @@
+#pragma once
 using namespace std;
 namespace bullpgia{
 
-class calculate{
 
-public:
-string calculateBullAndPgia(string choice, string guess);
-int getNumExactCharMatches(string chosen, string guess);
-int getNumSimilarChars(string chosen, string guess);
-int* getCharCounts(string str);
-};
-};
+ string calculateBullAndPgia(string choice, string guess);
+ int getNumExactCharMatches(string chosen, string guess);
+ int getNumSimilarChars(string chosen, string guess);
+ int* getCharCounts(string str);
+
+}

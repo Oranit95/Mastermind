68d4c9db3b79d06825cab1f11c6cc3b8d45a8d59
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-04-21 17:37:41 +0300

    Update Guesser.cpp

--- Guesser.cpp
+++ Guesser.cpp
@@ -1,7 +1,20 @@
 #include "Guesser.hpp"
+#include <string>
+using std::string;
 namespace bullpgia{
 
-Guesser:: virtual string guess(){
+uint length;
+string reply;
 
+string Guesser::virtual guess(){
+ return "";
 }
-};
+
+void Guesser::virtual startNewGame(uint length){
+ cout<<length<<endl;
+}
+
+void Guesser::virtual learn(string reply){
+ cout<<reply<<endl;
+}
+}

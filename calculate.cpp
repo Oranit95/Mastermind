9e3f77bb0835d078da13b4fff43c1da923c97296
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-04-30 14:19:50 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -8,53 +8,38 @@ namespace bullpgia{
 
   string calculateBullAndPgia(string choice, string guess){
 
-        if(choice == ""){
-		throw std::invalid_argument( "The string is empty" );
-	}
-	if(guess == ""){
-		throw std::invalid_argument( "The string is empty"  );
-	}
-	
-	//get the bulls
-	int bulls = getNumExactCharMatches(choice, guess);
-	//get the cows
-	int cows = getNumSimilarChars(choice, guess) - bulls;
-
-       //output
-        string s = "";
-        return s;
- }
-
-  int getNumExactCharMatches(string choice, string guess){
-	int count = 0;
-	int index = 0;
-	while(index < choice.length() && index < guess.length()){
-		if(choice[index] == guess[index]){
-			count++;
+	//if any of the strings are empty, the game cannot be played
+	if( choice == "" || guess == "" ) throw std::invalid_argument( "The string is empty" );
+		
+	int bulls = 0;
+	int cows = 0;
+		
+	//for the string selected by chooser
+	for( int i = 0; i < choice.length(); i++ ){
+		int runnerInt = choice[i];
+			
+		
+		for( int j = 0; j < guess.length(); j++ ){
+			int guessInt = guess[j];
+				
+				
+			if( runnerInt == guessInt){
+				if( i == j ){ //position match
+					bulls++;
+				}
+				else{ //contains relation
+					cows++;
+				}
+			}
 		}
-		index++;
 	}
-	return count;
- }
+		
+	string b = to_string(bulls);
+        string c = to_string(cows);
+        string s = b + " , " + c;
+        return s;
 
-  int getNumSimilarChars(string choice, string guess){
-	int* p1Counts = getCharCounts(choice);
-	int* p2Counts = getCharCounts(guess);
-	int count = 0;
-	for(int i = 0; i < 9; i++){
-		count+= min(p1Counts[i], p2Counts[i]);
 	}
 
-	return count;
- }
 
-  int* getCharCounts(string str){
-
-     int a = 2;
-     int *counts = &a;
-       
-	return counts;
-        
-        
-  }
 }

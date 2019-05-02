8d5c46743f0dee7a2a90724d5e11019d2f723aef
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-02 14:42:21 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,6 +1,8 @@
 #include "SmartGuesser.hpp"
 #include "Guesser.hpp"
 #include <string>
+#include <iostream>
+#include <sstream>
 using std::string;
 
 
@@ -8,6 +10,14 @@ string bullpgia::SmartGuesser::guess() {
  return "";
 }
 void bullpgia::SmartGuesser::startNewGame(uint length){
+
 }
 void bullpgia::SmartGuesser::learn(string reply){
+ 
+    int sign= reply.find(",");
+    int bulls = stod(reply.substr(0,sign));
+    int cows = stod(reply.substr(sign+1,reply.length()-1));
+    cout<<bulls;
+    cout<<cows;
+
 }

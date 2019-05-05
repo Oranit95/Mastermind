ee7ea9f70b1594ef42f7aee344f0ce640035dbcf
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-05 17:17:53 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -16,12 +16,12 @@ namespace bullpgia{
 		
 	//for the string selected by chooser
 	for( int i = 0; i < choice.length(); i++ ){
-		int runnerInt = choice[i];
+		char runnerInt = choice[i];
 		cout<<"runnerInt "<<runnerInt<<endl;
 			
 		
 		for( int j = 0; j < guess.length(); j++ ){
-			int guessInt = guess[j];
+			char guessInt = guess[j];
 			cout<<"guessInt "<<guessInt<<endl;	
 				
 			if( runnerInt == guessInt){

a6eeb5e7f267d8bde896f168c573c85adaef1bf4
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-05 18:08:17 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -10,40 +10,42 @@ namespace bullpgia{
 
 	//if any of the strings are empty, the game cannot be played
 	if( choice == "" || guess == "" ) throw std::invalid_argument( "The string is empty" );
-		
 	int bulls = 0;
 	int cows = 0;
-		
-	//for the string selected by chooser
+
 	for( int i = 0; i < choice.length(); i++ ){
-		char runnerInt = choice[i];
-		cout<<"runnerInt: "<<runnerInt<<endl;
-		
-		for( int j = 0; j < guess.length(); j++ ){
-			char guessInt = guess[j];
-			cout<<"guessInt: "<<guessInt<<endl;				
-				
-			if( runnerInt == guessInt){
-				if( i == j ){ //position match
-					bulls++;
-				}
-				else{ //contains relation
-					cows++;
-				}
-			}
+            if(choice[i] == guess[i]){
+            bulls++;
+            guess[i] = '!';
+            choice[i] = '!';
 		}
+          } 
+		
+	if(bulls == choice.length()){
+           string b = to_string(bulls);
+           string c = to_string(cows);
+           string s = b + "," + c;
+	   return s;
 	}
-		cout<<"bulls before: "<<bulls<<endl;
-	    cout<<"cows before: "<<cows<<endl;
+
+		for( int j = 0; j < choice.length(); j++){
+                 for(int k = 0; k < guess.length(); k++){
+			if(choice[j]==guess[k] && choice[j]!='!' && guess[k]!='!'){
+				cows++;
+                                guess[k]='!';
+                                break;
+                      }				
+			
+		}      
+
+	}
+	
 	string b = to_string(bulls);
-	  cout<<"BULLS ARE "<<b<<endl;
         string c = to_string(cows);
-	  cout<<"COWS ARE "<<c<<endl;
         string s = b + "," + c;
-	  cout<<"String is "<<s<<endl;
+
         return s;
 
 	}
-
 
 }
